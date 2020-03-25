a6080b19937b7a84f675c2e841dceade3422f414
Jennifer Mullin <jennifer.lindsey.mullin@gmail.com>
2020-03-24 18:17:17 -0600

    update

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,14 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
-var connection;
 
-if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnnection(process.env.JAWSDB_URL);
-} else {
-  connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "Olive9139!",
-    database: "burgers_db"
-  });
-}
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Olive9139!",
+  database: "burgers_db"
+});
 
 // Make connection.
 connection.connect(function (err) {
