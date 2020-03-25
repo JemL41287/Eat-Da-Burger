7e5c52a4e668a5ba3798dc92d6ecc25e976644ef
Jennifer Mullin <jennifer.lindsey.mullin@gmail.com>
2020-03-24 18:11:57 -0600

    change

--- config/orm.js
+++ config/orm.js
@@ -38,10 +38,8 @@ function objToSql(ob) {
 var orm = {
   selectAll: function(tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
-    connection.query(queryString, function(err, result) {
-      if (err) {
-        throw err;
-      }
+    connection.query(queryString, function(result) {
+ 
       cb(result);
     });
   },
