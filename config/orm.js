1b35c6adb505da0db8f02dbd48fd50bb8cc42110
Jennifer Mullin <jennifer.lindsey.mullin@gmail.com>
2020-03-26 17:30:49 -0600

    fixed orm.js

--- config/orm.js
+++ config/orm.js
@@ -39,6 +39,9 @@ var orm = {
   selectAll: function(tableInput, cb) {
     var queryString = "SELECT * FROM " + tableInput + ";";
     connection.query(queryString, function(result) {
+      if (err) {
+        throw err;
+      }
  
       cb(result);
     });
